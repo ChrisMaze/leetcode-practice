33596f10fa045a3aabcdbafeb1116e6699f8913f
zerong <chdmazr@126.com>
2021-10-11 00:28:45 +0800

    79: [word search] format

--- js/src/79-WordSearch/wordSearch.ts
+++ js/src/79-WordSearch/wordSearch.ts
@@ -1,13 +1,14 @@
 const DFS = (board: string[][], word: string, i: number, j: number, n: number): boolean => {
-  if (i < 0 || j < 0 || i + 1 > board.length || j + 1 > board[i].length || board[i][j] !== word[n]) return false
-  if (word.length === n+1) {
+  if (i < 0 || j < 0 || i + 1 > board.length || j + 1 > board[i].length || board[i][j] !== word[n]) return false;
+  if (word.length === n + 1) {
     return true;
   }
   board[i][j] = '0';
-  const status: boolean = DFS(board, word, i+1, j, n+1) ||
-    DFS(board, word, i-1, j, n+1) ||
-    DFS(board, word, i, j+1, n+1) ||
-    DFS(board, word, i, j-1, n+1);
+  const status: boolean =
+    DFS(board, word, i + 1, j, n + 1) ||
+    DFS(board, word, i - 1, j, n + 1) ||
+    DFS(board, word, i, j + 1, n + 1) ||
+    DFS(board, word, i, j - 1, n + 1);
 
   board[i][j] = word[n];
   return status;
